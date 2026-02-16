c30ec6b2cfdd4a884db48689490d2b0fde06ee1e
vlado-manov <vladimir.manov.bg@gmail.com>
2026-02-16 22:43:49 +0200

    images

---
+++ assets/audio.ts
@@ -0,0 +1,25 @@
+// assets/audio/character_sounds.ts
+import { AVPlaybackSource } from "expo-av";
+
+export const character_sounds: Record<string, AVPlaybackSource> = {
+  allan: require("./audio/heroPicker/allan.wav"),
+  booena: require("./audio/heroPicker/booena.wav"),
+  brochain: require("./audio/heroPicker/brochain.mp3"),
+  dadGpt: require("./audio/heroPicker/dadGpt.wav"),
+  dubai: require("./audio/heroPicker/dubai.mp3"),
+  drWrong: require("./audio/heroPicker/drWrong.mp3"),
+  hangreta: require("./audio/heroPicker/hangreta.mp3"),
+  monday: require("./audio/heroPicker/monday.wav"),
+  screena: require("./audio/heroPicker/screena.wav"),
+  susie: require("./audio/heroPicker/susie.wav"),
+  simpalot: require("./audio/heroPicker/simpalot.wav"),
+  tedimechov: require("./audio/heroPicker/tedimechov.wav"),
+  vanessa: require("./audio/heroPicker/vanessa.mp3"),
+  virala: require("./audio/heroPicker/virala.wav"),
+  winebender: require("./audio/heroPicker/winebender.wav"),
+  retrograda: require("./audio/heroPicker/retrograda.wav"),
+  xGlitch: require("./audio/heroPicker/xGlitch.wav"),
+  uncleVape: require("./audio/heroPicker/uncleVape.wav"),
+  pluggedInPete: require("./audio/heroPicker/pluggedInPete.wav"),
+  mrGoodTime: require("./audio/heroPicker/mrGoodTime.mp3"),
+};
---
+++ assets/lottie.ts
@@ -0,0 +1,4 @@
+export const lottie = {
+  confettiTop: require("./lottie/confetti-top.json"),
+  count: require("./lottie/count.json"),
+};
